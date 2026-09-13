216717faf16c5e9459c0aaf1e58a861dfa773639
telio-s <telio.s@hotmail.com>
2026-09-13 22:34:07 +0700

    feed: product into db

---
+++ api/db/migrations/000003_seed_products.down.sql
@@ -0,0 +1,2 @@
+DELETE FROM products
+WHERE name IN ('Red', 'Green', 'Blue', 'Yellow', 'Pink', 'Purple', 'Orange');
---
+++ api/db/migrations/000003_seed_products.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (name, price) VALUES
+    ('Red', '50'),
+    ('Green', '40'),
+    ('Blue', '30'),
+    ('Yellow', '50'),
+    ('Pink', '80'),
+    ('Purple', '90'),
+    ('Orange', '120');
